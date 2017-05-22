1bc2a2b906a6c5c4453797d917b17750f6db49d0
Vivian Zhang <vivian.mzh@gmail.com>
2017-05-21 23:31:57 -0700

    Fix fade in and clean up single quotes

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
+
+  $(".intro-fade-in").animate({"opacity": "1", "paddingTop": 0}, 1500);
+
   $(window).scroll(function() {
-    $('.fade-in').each(function(item) {
+    $(".fade-in").each(function(item) {
       var bottom_of_object = $(this).offset().top + $(this).outerHeight();
       var bottom_of_window = $(window).scrollTop() + $(window).height();
-      /* If the object is completely visible in the window, fade it it */
+      /* If half of the object is completely visible in the window, fade it */
       if( bottom_of_window > bottom_of_object/2 ){
-          $(this).animate({'opacity':'1'},2000);   
+          $(this).animate({"opacity":"1"}, 1500);   
       }
     })
-
-    $('.title').css('color', 'green');
   })
+
 });
